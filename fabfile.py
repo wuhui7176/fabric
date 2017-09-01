d3df14e03c574698252c3bbee72ff90c0ff30827
wuhui <wuhui7176@qq.com>
2017-09-02 00:37:54 +0800

    test

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+from fabric.api import *
+from datetime import datetime
+
+
+env.user="root"
+env.host=['47.94.137.148']
+env.password="Xxx5211314"
+env.port=22
+
+def hello():
+    remote_tmp_tar="/opt"
+    run('rm -f %s' % remote_tmp_tar)
+
+
+def local_test():
+    local("ls -la")
